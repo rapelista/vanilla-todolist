6b44094e356875c410ff9d99bd389fae2972b4e2
Faturrahman Ikhsan Amin <faturrhmnikhsan@gmail.com>
2024-07-21 23:16:01 +0700

    vanilla todolist

---
+++ main.js
@@ -0,0 +1,21 @@
+const listContainer = document.querySelector(".listContainer");
+const inputTodo = document.getElementById("inputTodo");
+const button1 = document.getElementById("button1");
+
+button1.addEventListener("click", () => {
+  if (inputTodo.value) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = inputTodo.value;
+    listContainer.appendChild(paragraph);
+    inputTodo.value = "";
+    paragraph.addEventListener("click", () => {
+      paragraph.classList.toggle("p-through");
+    });
+
+    paragraph.addEventListener("dblclick", () => {
+      listContainer.removeChild(paragraph);
+    });
+  } else {
+    alert("Add Value To The Form!");
+  }
+});
